63547bdcda5f3b1297516af9ca027e85cd21956a
Mslick <nanthapopguitarist@gmail.com>
2022-09-24 14:47:24 +0700

    update

--- Q4/forward_backward_print.c
+++ Q4/forward_backward_print.c
@@ -1,5 +1,22 @@
 #include <stdio.h>
 
 void main(){
-    printf("Hello World\n");
+    int N; scanf("%d", &N);
+    int pp[5000];
+    int i;
+    int M;
+    for (i = 0 ; i < N ; i++){
+        scanf("%d", &pp[i]);
+    }
+    scanf("%d", &M);
+    if (M > 0){
+        for (i = 0 ; i < N ; i++){
+            printf("%d ", pp[i] + M);
+        }
+    }
+    else if (M < 0){
+        for (i = N - 1 ; i >= 0 ; i--){
+            printf("%d ", pp[i] + M);
+        }
+    }
 }
